d8a5b48af87fb5366c70382940c48677bba83f5b
Nick <113937068+KlevNik@users.noreply.github.com>
2023-11-01 22:12:50 +0300

    all

--- src/main/java/ru/nic/project1/models/Person.java
+++ src/main/java/ru/nic/project1/models/Person.java
@@ -1,4 +1,52 @@
 package ru.nic.project1.models;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
+
+    private int id;
+
+    @NotEmpty(message = "Поле не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов длиной")
+    private String fullName;
+
+    @Min(value = 1950, message = "Год рождения должен быть больше, чем 1950г")
+    private int yearOfBirth;
+
+    // Конструктор по умолчанию нужный для Spring (тут он должен быть пустым, потому что потом дергает геттерами и сеттерами)
+    public Person() {
+
+    }
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
 }
